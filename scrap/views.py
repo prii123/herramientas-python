e8956302e607738523bc9258723b386015c65048
prii123 <printsvallejos@gmail.com>
2023-05-23 23:02:17 -0500

    eliminar carpetas dist

--- scrap/views.py
+++
@@ -1,9 +0,0 @@
-from django.shortcuts import render
-from django.http import HttpResponse
-# Create your views here.
-def hello(request):
-    return HttpResponse("Hola mundo")
-
-
-def about(request):
-    return HttpResponse("about")
